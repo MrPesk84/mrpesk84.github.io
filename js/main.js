85ddac77eab559d42bee6c20117a5834b32968ec
Toni <toni@gmail.com>
2025-11-24 02:03:27 +0100

    Initial web

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Archivo para la lógica de la aplicación.
+// Por ahora, se deja vacío.
+// Más adelante se añadirá el código para manejar los clics en el menú
+// y cargar el contenido dinámicamente.
+
+console.log("main.js cargado.");
